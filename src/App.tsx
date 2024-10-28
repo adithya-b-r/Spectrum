84f9d2de14004fa40d2bc5a6207da3c300833c3e
Abr009 <bradithya945@gmail.com>
2024-10-28 13:43:40 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, MUI!</h1>
+      <Button variant="contained" color="secondary">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
