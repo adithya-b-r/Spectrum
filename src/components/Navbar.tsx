edeb908e6140bd2bef627bae6c29a03d1f3876c8
Abr009 <bradithya945@gmail.com>
2024-10-31 09:18:10 +0530

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ export const Navbar = () => {
     <nav className="font-serif border-b-2  border-b-gray-200">
       <div className="p-3 flex justify-between items-center">
         <div className="flex items-center text-2xl font-bold">
-          <img src="/logo2.png" className="hidden md:flex w-12 h-12 rounded-full mr-2 cursor-pointer" />
-          <h1 className='tracking-wider text-3xl text-center hover:scale-105 transition-transform duration-1000 cursor-pointer'>Spectrum</h1>
+          <img src="/logo2.png" className="hidden md:flex size-12 rounded-full mr-2 cursor-pointer" />
+          <h1 className='tracking-wider text-2xl md:text-3xl text-center hover:scale-105 transition-transform duration-1000 cursor-pointer'>Spectrum</h1>
 
           <div className="relative hidden md:flex ml-4 items-center justify-center">
             <i className='bx bx-search absolute left-5 text-xl text-gray-500'></i>
@@ -12,10 +12,10 @@ export const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex items-center text-3xl justify-center gap-4 sm:gap-8 md:gap-10">
+        <div className="flex items-center text-2xl md:text-3xl justify-center gap-4 sm:gap-8 md:gap-10">
           <i className="bx bx-edit font-thin cursor-pointer"></i>
           <i className="bx bx-bell cursor-pointer"></i>
-          <img src="/profile.jpg" className="w-10 h-10 rounded-full cursor-pointer" />
+          <img src="/profile.jpg" className="size-8 rounded-full cursor-pointer" />
         </div>
       </div>
     </nav>
