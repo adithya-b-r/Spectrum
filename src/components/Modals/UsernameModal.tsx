4437fc1368765263aca3e64e908cdc93b2354449
Abr009 <bradithya945@gmail.com>
2024-11-01 13:34:47 +0530

    Added Modal for Username

--- src/components/Modals/UsernameModal.tsx
+++ src/components/Modals/UsernameModal.tsx
@@ -1,25 +1,19 @@
-export const UsernameModal = () => {
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const UsernameModal = ({ isOpen, onClose }: ModalProps) => {
+
   return (
-    <div className="absolute z-50 flex items-center justify-center w-full h-screen bg-gray-600 bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-80">
-        {/* Close Button */}
-        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 font-bold">
-          [X]
-        </button>
-        
-        {/* Modal Heading */}
+    <div onClick={onClose} className={`${!isOpen ? 'hidden' : ''} fixed inset-0 z-50 flex items-center justify-center w-full h-screen bg-gray-600 bg-opacity-50`}>
+      <div onClick={(e) => e.stopPropagation()} className="bg-white rounded-lg shadow-lg p-6 w-80">
         <h2 className="text-xl font-semibold text-gray-800 mb-4">Username</h2>
-        
-        {/* Input Field */}
-        <input
-          type="text"
-          placeholder="Enter your username"
-          className="w-full border border-gray-300 rounded-md p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        
-        {/* Buttons */}
+
+        <input id="username" type="text" placeholder="Enter new username" className="w-full border border-gray-300 rounded-md p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
         <div className="flex justify-end gap-4">
-          <button className="px-4 py-2 bg-gray-300 rounded-md text-gray-700 hover:bg-gray-400">Cancel</button>
+          <button onClick={onClose} className="px-4 py-2 bg-gray-300 rounded-md text-gray-700 hover:bg-gray-400">Cancel</button>
           <button className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">Save</button>
         </div>
       </div>
--- src/pages/Profile/Sections/Account.tsx
+++ src/pages/Profile/Sections/Account.tsx
@@ -1,28 +1,41 @@
+import { useState } from "react"
+import { UsernameModal } from "../../../components/Modals/UsernameModal"
+
 export const Account = () => {
+  const [userModalState, setUserModalState] = useState(false)
+
+  const toggleUserModalState = () => {
+    setUserModalState((prev) => !prev)
+  }
+
   return (
-      <div className="flex-col h-fit w-full p-4 overflow-scroll">
-        <div className="flex justify-between py-4 cursor-pointer">
-          <p className="text-lg font-semibold text-gray-800">Name</p>
-          <span className="hover:text-gray-600">Nobita Nobi</span>
-        </div>
-        <div className="flex justify-between py-4 cursor-pointer">
-          <p className="text-lg font-semibold text-gray-800">Username</p>
-          <span className="hover:text-gray-600">@nobita_nobi</span>
-        </div>
-        <div className="flex flex-wrap justify-between py-4 cursor-pointer">
-          <p className="text-lg font-semibold text-gray-800">Email</p>
-          <span className="hover:text-gray-600">nobitanobi@gmail.com</span>
-        </div>
-        <div className="flex-col  py-4 cursor-pointer">
-          <p className="text-lg font-semibold text-gray-800">About</p>
-          <span className="hover:text-gray-600">Hello, I'm Nobita dedicated, creative individual, committed to supporting my friends with loyalty and care.</span>
-        </div>
-        <div className="flex flex-col sm:flex-row justify-between py-4">
-          <p className="text-lg font-semibold text-gray-800">Change Password</p>
-          <button className="py-2 px-3 mt-2 sm:mt-0 rounded-md font-semibold bg-blue-500 text-white cursor-pointer">
-            New Password
-          </button>
-        </div>
+    <div className="flex-col h-fit w-full p-4 overflow-scroll">
+      {userModalState && (<UsernameModal isOpen={true} onClose={toggleUserModalState}/>)}
+      
+      <div className="flex justify-between py-4 cursor-pointer">
+        <p className="text-lg font-semibold text-gray-800">Name</p>
+        <span className="hover:text-gray-600">Nobita Nobi</span>
       </div>
+      <div className="flex justify-between py-4 cursor-pointer" onClick={toggleUserModalState}>
+        <p className="text-lg font-semibold text-gray-800">Username</p>
+        <span className="hover:text-gray-600">@nobita_nobi</span>
+      </div>
+      <div className="flex flex-wrap justify-between py-4 cursor-pointer">
+        <p className="text-lg font-semibold text-gray-800">Email</p>
+        <span className="hover:text-gray-600">nobitanobi@gmail.com</span>
+      </div>
+      <div className="flex-col  py-4 cursor-pointer">
+        <p className="text-lg font-semibold text-gray-800">About</p>
+        <span className="hover:text-gray-600">Hello, I'm Nobita dedicated, creative individual, committed to supporting my friends with loyalty and care.</span>
+      </div>
+      <div className="flex flex-col sm:flex-row justify-between py-4">
+        <p className="text-lg font-semibold text-gray-800">Change Password</p>
+        <button className="py-2 px-3 mt-2 sm:mt-0 rounded-md font-semibold bg-blue-500 text-white cursor-pointer">
+          New Password
+        </button>
+      </div>
+
+      <h1>{userModalState} hello</h1>
+    </div>
   )
 }
