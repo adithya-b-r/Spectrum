32baded896fb44507fad313e0134eeafa77535df
Abr009 <bradithya945@gmail.com>
2024-11-02 11:48:04 +0530

    Fix(Navbar): close dropdown on navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,24 +35,24 @@ export const Navbar = () => {
 
           {displayNav && (
             <div className="absolute flex flex-col top-16 md:top-20 right-3 bg-white w-56 rounded-md shadow-slate-200 border-2 border-slate-100 shadow-xl z-40 select-none">
-              <div onClick={() => navigate("/profile")} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
+              <div onClick={() => { setdisplayNav(false); navigate("/profile"); }} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
                 <i className="bx bx-user mr-2 text-xl"></i>
                 <p className="text-xl">Profile</p>
               </div>
-              <div onClick={() => navigate("/blogs")} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
+              <div onClick={() => { setdisplayNav(false); navigate("/blogs"); }} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
                 <i className="bx bx-book mr-2 text-xl"></i>
                 <p className="text-xl">My Blogs</p>
               </div>
-              <div onClick={() => navigate("/saved")} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
+              <div onClick={() => { setdisplayNav(false); navigate("/saved"); }} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
                 <i className="bx bx-bookmark mr-2 text-xl"></i>
                 <p className="text-xl">Saved Blogs</p>
               </div>
               <div className="divider bg-slate-200 h-0.5 mx-3"></div>
-              <div onClick={() => navigate("/notifications")} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
+              <div onClick={() => { setdisplayNav(false); navigate("/notifications"); }} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
                 <i className="bx bx-bell mr-2 text-xl"></i>
                 <p className="text-xl">Notifications</p>
               </div>
-              <div onClick={() => navigate("/logout")} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
+              <div onClick={() => { setdisplayNav(false); navigate("/logout"); }} className="flex items-center py-4 px-4 hover:bg-gray-100 cursor-pointer">
                 <i className="bx bx-log-out mr-2 text-xl"></i>
                 <p className="text-xl">Logout</p>
               </div>
