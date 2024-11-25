82d0f859ec415a11d0bc6aebe015c42299dfedf5
Abr009 <bradithya945@gmail.com>
2024-11-25 10:59:22 +0530

    Added Responsive for Landing page

--- src/pages/LandingPage/LandingNavbar.tsx
+++ src/pages/LandingPage/LandingNavbar.tsx
@@ -8,10 +8,10 @@ export const LandingNavbar = () => {
         </div>
 
         <div className="flex items-center text-2xl md:text-3xl justify-center gap-4 sm:gap-8 md:gap-4 tracking-wider">
-          <p style={{fontSize: "1rem"}} className="cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">About Us</p>
-          <p style={{fontSize: "1rem"}} className="cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">Write</p>
-          <p style={{fontSize: "1rem"}} className="cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">Sign In</p>
-          <p style={{fontSize: "1rem"}} className="cursor-pointer bg-black text-white py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">Get started</p>
+          <p style={{fontSize: "1rem"}} className="md:block hidden cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">About Us</p>
+          <p style={{fontSize: "1rem"}} className="md:block hidden cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">Write</p>
+          <p style={{fontSize: "1rem"}} className="md:block hidden cursor-pointer font-semibold py-1 px-4 rounded-full hover:scale-110 duration-500 transition-transform">Sign In</p>
+          <p style={{fontSize: "1rem"}} className="cursor-pointer bg-black text-white py-1 px-4 rounded-full hover:scale-105 duration-500 transition-transform">Get Started</p>
         </div>
       </div>
     </nav>
