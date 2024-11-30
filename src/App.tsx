9179d2b40d9d8b0441c0fd1c2e7dd115ee5061bb
Abr009 <bradithya945@gmail.com>
2024-11-30 21:31:25 +0530

    Added Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Favorites } from './pages/Favorites/Favorites';
 import { Profile } from './pages/Profile/Profile';
 import { CreatePost } from './pages/Write/CreatePost';
 import { ToastNotification } from './components/Toast/ToastNotification'
-import { Landing } from './pages/LandingPage/landing';
 
 interface AppContextType {
   theme: string;
@@ -29,14 +28,13 @@ function App() {
   return (
     <AppContext.Provider value={{ theme, setTheme, notificationPage, setNotificationPage }}>
       <BrowserRouter>
-      <ToastNotification />
-        {location.pathname !== '/land' && <Navbar />}
+        <ToastNotification />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/land" element={<Landing />} />
           <Route path="/*" element={<h3 className='text-3xl w-full my-10 font-bold text-center'>🛠️Under Construction🛠️</h3>} />
         </Routes>
       </BrowserRouter>
