1f125214f77047dd4436195d8e87b2b2c2a10e7b
Abr009 <bradithya945@gmail.com>
2025-01-31 20:50:12 +0530

    Added vercel.json to specify deployment subdirectory.

--- client/src/pages/Blog/Sections/BlogPost.tsx
+++ client/src/pages/Blog/Sections/BlogPost.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
-
 export const BlogPost = () => {
-  const [Following, setFollowing] = useState(false);
-
-  const toggleFollow = () => {
-    setFollowing(!Following);
-  };
-
   return (
     <div className="mx-[20%] w-50 h-screen flex flex-col border-x-2">
       <div className="m-5">
-        <h1 className="text-4xl font-extrabold leading-snug text-gray-700">Unlocking the Secrets to Success: Practical Tips for Everyday Life</h1>
+        <h1 className="text-4xl font-extrabold leading-snug">Unlocking the Secrets to Success: Practical Tips for Everyday Life</h1>
       </div>
 
       <div className="mx-5">
