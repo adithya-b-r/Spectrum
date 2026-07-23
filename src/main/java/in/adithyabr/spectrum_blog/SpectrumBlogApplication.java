1fa2e4bbaba5192a55386cd7fdd2996ff4f2a407
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2026-07-23 08:07:31 +0530

    Initial commit

---
+++ src/main/java/in/adithyabr/spectrum_blog/SpectrumBlogApplication.java
@@ -0,0 +1,12 @@
+package in.adithyabr.spectrum_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectrumBlogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpectrumBlogApplication.class, args);
+  }
+}
