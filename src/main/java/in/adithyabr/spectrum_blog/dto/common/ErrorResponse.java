a812d7ce5ceed1cfe7b895af340c0d257749a2b5
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2026-07-31 19:29:15 +0530

    feat(api): add common response contracts

---
+++ src/main/java/in/adithyabr/spectrum_blog/dto/common/ErrorResponse.java
@@ -0,0 +1,22 @@
+package in.adithyabr.spectrum_blog.dto.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+  private int status;
+  private String message;
+  private Map<String, String> errors;
+  private LocalDateTime timestamp;
+}
---
+++ src/main/java/in/adithyabr/spectrum_blog/dto/common/MessageResponse.java
@@ -0,0 +1,12 @@
+package in.adithyabr.spectrum_blog.dto.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageResponse {
+  private String message;
+}
