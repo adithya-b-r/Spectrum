2e77f5e77858089059733749c5b28d6934d8b5bd
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2026-08-13 09:29:10 +0530

    feat(comments): add authenticated story comments

--- src/main/java/in/adithyabr/spectrum_blog/service/blog/BlogService.java
+++ src/main/java/in/adithyabr/spectrum_blog/service/blog/BlogService.java
@@ -1,6 +1,7 @@
 package in.adithyabr.spectrum_blog.service.blog;
 
 import in.adithyabr.spectrum_blog.dto.blog.*;
+import in.adithyabr.spectrum_blog.dto.comment.CommentResponse;
 import in.adithyabr.spectrum_blog.entity.blog.Blog;
 
 public interface BlogService {
@@ -10,4 +11,5 @@ public interface BlogService {
   BlogListResponse getUserBlogs(String userParam, int page, int limit);
   BlogResponse updateBlog(Integer authUserId, Integer blogId, UpdateBlogRequest request);
   void deleteBlog(Integer authUserId, Integer blogId);
+  CommentResponse addComment(Integer currentUserId, Integer blogId, String content);
 }
